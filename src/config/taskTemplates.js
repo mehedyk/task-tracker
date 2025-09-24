02731017f7dc2acb07230eabfc7263742f7a54bd
S.M. Mehedy Kawser <mahdi.kawser@gmail.com>
2025-09-24 13:56:19 +0600

    Create taskTemplates.js

---
+++ src/config/taskTemplates.js
@@ -0,0 +1,115 @@
+// src/config/taskTemplates.js
+export const taskTemplates = [
+  {
+    id: 'salah',
+    name: 'Salah (At Least Qaza)',
+    type: 'expandable',
+    icon: '🕌',
+    subtasks: [
+      { id: 'fajr', name: 'Fajr', completed: false },
+      { id: 'dhuhr', name: 'Dhuhr', completed: false },
+      { id: 'asr', name: 'Asr', completed: false },
+      { id: 'maghrib', name: 'Maghrib', completed: false },
+      { id: 'isha', name: 'Isha', completed: false }
+    ]
+  },
+  {
+    id: 'addictions',
+    name: 'Stop Addictions and Porn Content',
+    type: 'simple',
+    icon: '🚫',
+    completed: false
+  },
+  {
+    id: 'coding',
+    name: 'Code/IT Stuff',
+    type: 'expandable_with_links',
+    icon: '💻',
+    subtasks: [
+      { 
+        id: 'beecrowd', 
+        name: 'BeeCrowd', 
+        completed: false,
+        link: 'https://github.com/mehedyk/100-BeeCrowd-Problems'
+      },
+      { 
+        id: 'hackerrank', 
+        name: 'HackerRank', 
+        completed: false,
+        link: 'https://github.com/mehedyk/100-HackerRank-Problems'
+      },
+      { 
+        id: 'codeforces', 
+        name: 'CodeForces', 
+        completed: false,
+        link: 'https://github.com/mehedyk/100-CodeForces-Problems'
+      }
+    ]
+  },
+  {
+    id: 'islam',
+    name: 'Learn Core Islam',
+    type: 'simple',
+    icon: '📖',
+    completed: false
+  },
+  {
+    id: 'fitness',
+    name: 'Fitness',
+    type: 'expandable',
+    icon: '💪',
+    subtasks: [
+      { id: 'running', name: 'Running (30 mins)', completed: false },
+      { id: 'water', name: 'Drink 8 Glasses of Water', completed: false },
+      { id: 'pushups', name: '20 Push-ups', completed: false },
+      { id: 'stretching', name: 'Stretching (15 mins)', completed: false },
+      { id: 'meditation', name: 'Meditation (10 mins)', completed: false }
+    ]
+  }
+];
+
+export const createDailyTasksFromTemplates = (userId, date) => {
+  const tasks = [];
+  
+  taskTemplates.forEach(template => {
+    if (template.type === 'simple') {
+      tasks.push({
+        user_id: userId,
+        task_id: template.id,
+        task_name: template.name,
+        task_type: 'simple',
+        date: date,
+        completed: false,
+        icon: template.icon
+      });
+    } else {
+      // Create main task
+      tasks.push({
+        user_id: userId,
+        task_id: template.id,
+        task_name: template.name,
+        task_type: template.type,
+        date: date,
+        completed: false,
+        icon: template.icon,
+        is_parent: true
+      });
+      
+      // Create subtasks
+      template.subtasks.forEach(subtask => {
+        tasks.push({
+          user_id: userId,
+          task_id: `${template.id}_${subtask.id}`,
+          task_name: subtask.name,
+          task_type: 'subtask',
+          parent_id: template.id,
+          date: date,
+          completed: false,
+          link: subtask.link || null
+        });
+      });
+    }
+  });
+  
+  return tasks;
+};
